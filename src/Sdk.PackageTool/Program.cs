67e7b20fbbddb8c4fb37a40d8b0922463d399cc8
Talen Fisher <talen.fisher@cythral.com>
2022-02-07 16:06:13 -0600

    Publish package tool as a .NET tool

--- src/Sdk.PackageTool/Program.cs
+++ src/Sdk.PackageTool/Program.cs
@@ -1,9 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Loader;
 
 using Brighid.Commands.Sdk.PackageTool;
 
@@ -15,67 +10,30 @@ using Serilog;
 
 #pragma warning disable SA1516 // Elements separated by blank line
 
-SetupDependencyResolver();
-Run(args);
-
-static void SetupDependencyResolver()
+Host.CreateDefaultBuilder()
+.UseConsoleLifetime()
+.UseSerilog(dispose: true)
+.ConfigureLogging(options =>
 {
-    var additionalProbingPath = Environment.GetEnvironmentVariable("ADDITIONAL_PROBING_PATH")!;
-    var dllsInProbingPath = from file in Directory.GetFiles(additionalProbingPath, $"*.dll", SearchOption.AllDirectories)
-                            where file.Contains("netstandard") || file.Contains("net6.0") || file.Contains("net5.0")
-                            select file;
-
-    AssemblyLoadContext.Default.Resolving += (_, name) =>
+    options.AddSimpleConsole(consoleOptions =>
     {
-        var query = from dll in dllsInProbingPath
-                    where dll.EndsWith($"{name.Name}.dll", true, null)
-                    let assembly = TryLoadAssembly(dll)
-                    where assembly != null
-                    select assembly;
-
-        return query.FirstOrDefault();
-    };
-}
-
-static Assembly? TryLoadAssembly(string path)
+        consoleOptions.SingleLine = true;
+    });
+})
+.ConfigureHostConfiguration(options =>
 {
-    try
+    if (args.Length > 1)
     {
-        return Assembly.LoadFile(path);
+        options.AddCommandLine(args[1..], new Dictionary<string, string>
+        {
+            ["--bucket-name"] = "CommandLine:S3BucketName",
+            ["--destination"] = "CommandLine:TemplateDestination",
+        });
     }
-    catch (Exception)
-    {
-        return null;
-    }
-}
-
-static void Run(string[] args)
+})
+.ConfigureServices((context, services) =>
 {
-    Host.CreateDefaultBuilder()
-        .UseConsoleLifetime()
-        .UseSerilog(dispose: true)
-        .ConfigureLogging(options =>
-        {
-            options.AddSimpleConsole(consoleOptions =>
-            {
-                consoleOptions.SingleLine = true;
-            });
-        })
-        .ConfigureHostConfiguration(options =>
-        {
-            if (args.Length > 1)
-            {
-                options.AddCommandLine(args[1..], new Dictionary<string, string>
-                {
-                    ["--bucket-name"] = "CommandLine:S3BucketName",
-                    ["--destination"] = "CommandLine:TemplateDestination",
-                });
-            }
-        })
-        .ConfigureServices((context, services) =>
-        {
-            new Startup(context.Configuration).ConfigureServices(services);
-        })
-        .Build()
-        .Run();
-}
+    new Startup(context.Configuration).ConfigureServices(services);
+})
+.Build()
+.Run();
